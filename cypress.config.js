3400793729092ca4e8d779fb88f90b45a55f4685
PeterKuzyk <55903558+PeterKuzyk@users.noreply.github.com>
2025-05-30 16:48:01 -0400

    updates

--- cypress.config.js
+++ cypress.config.js
@@ -4,17 +4,19 @@ const watch = require('cypress-watch-and-reload/plugins'); // Import the plugin
 
 module.exports = defineConfig({
   // it can override default 4000 ms to time you need, works globally, not the best solution
-  //defaultCommandTimeout: 6000,
+  defaultCommandTimeout: 6000,
 
   e2e: {
     setupNodeEvents(on, config) {
       // Tell the plugin which application files/patterns to watch
-      config.env['cypress-watch-and-reload'] = {
-        watch: ['support/**/*', 'integration/**/*', 'e2e/**/*'], // ADJUST THESE PATHS to your application's source code
-      };
-      // Apply the plugin's setupNodeEvents
-      return watch(on, config);
+      // config.env['cypress-watch-and-reload'] = {
+      //   watch: ['support/**/*', 'integration/**/*', 'e2e/**/*'], // ADJUST THESE PATHS to your application's source code
+      // };
+      // // Apply the plugin's setupNodeEvents
+      // return watch(on, config);
     },
-    specPattern: 'cypress/integration/examples/*.js'
+
+    specPattern: 'cypress/integration/examples/*.js',
+    watchForFileChanges: false,
   },
 });
