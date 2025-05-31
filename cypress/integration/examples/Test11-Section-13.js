7a76368655c06dafd361237daf8a14182bf79f97
PeterKuzyk <55903558+PeterKuzyk@users.noreply.github.com>
2025-05-31 15:11:22 -0400

    added Jenkins

---
+++ cypress/integration/examples/Test11-Section-13.js
@@ -0,0 +1,15 @@
+describe('Scripting commands', () => {
+
+    it('Jenkins CI', function () {
+        cy.log('This is just a placeholder test. No real logic here.');
+
+        /* added commands to package.json
+        "reporter": "npx cypress run",
+        "chrome": "npx cypress open --e2e --browser chrome",
+        "recordDashBoard": "npx cypress run --record --key .........."
+        */
+
+        // Jenkins test results in reporter
+        // http://localhost:63342/CypressAutomationTesting/cypress/reports/html/index.html?_ijt=30ss5or1cn9kb46auvee22vm2n&_ij_reload=RELOAD_ON_SAVE
+    });
+});
