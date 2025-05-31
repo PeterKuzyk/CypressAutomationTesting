5570aab148ba9088ee587163f2a0ce5450ee7e75
PeterKuzyk <55903558+PeterKuzyk@users.noreply.github.com>
2025-05-30 21:34:53 -0400

    Cypress Dashboard config, work on your onwn
    Co-authored-by: Nataliya Kuzyk <natalie87wass@gmail.com>

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 const watch = require('cypress-watch-and-reload/plugins'); // Import the plugin
 
 module.exports = defineConfig({
+  projectId: 'kgy48c',
   // it can override default 4000 ms to time you need, works globally, not the best solution
   defaultCommandTimeout: 6000,
   // mochawesome-reporter configs
@@ -16,6 +17,11 @@ module.exports = defineConfig({
 
   env: {
     url: "https://rahulshettyacademy.com",
+  },
+
+  retries: {
+    runMode: 1,
+
   },
 
   e2e: {
---
+++ cypress/integration/examples/Test10-Section-13.js
@@ -0,0 +1,5 @@
+describe('Cypress Dashboard', () => {
+    it('Personal Practice', () => {
+    // Cypress Dashboard practise alone with teacher, no public data to show
+    })
+})
